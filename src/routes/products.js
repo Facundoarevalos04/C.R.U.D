125dcdc15db1455ee5e00dfcea9f36f297b58f61
facundoarevalos04 <facundoarevalos04@gmail.com>
2022-09-14 13:16:00 -0300

    TP Crud 1-2

---
+++ src/routes/products.js
@@ -0,0 +1,32 @@
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+
+// ************ Middleware Require ************
+
+const {uploadImageProduct} =require('../middlewares/uploadFiles')
+
+// ************ Controller Require ************
+const productsController = require('../controllers/productsController');
+
+/*** GET ALL PRODUCTS ***/ 
+router.get('/', productsController.index); 
+
+/*** CREATE ONE PRODUCT ***/ 
+router.get('/create', productsController.create); 
+router.post('/store', uploadImageProduct.single('image') ,productsController.store); 
+
+
+/*** GET ONE PRODUCT ***/ 
+router.get('/detail/:id', productsController.detail); 
+
+/*** EDIT ONE PRODUCT ***/ 
+router.get('/edit/:id', productsController.edit); 
+router.put('/update/:id', productsController.update); 
+
+
+/*** DELETE ONE PRODUCT***/ 
+router.delete('/delete/:id', productsController.destroy); 
+
+
+module.exports = router;
